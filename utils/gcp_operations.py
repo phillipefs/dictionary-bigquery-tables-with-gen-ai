bcc6fd6c0f118ad1ef889848361daddb50df9f6b
Phillipe Santos <phillipefs@msn.com>
2024-04-14 08:42:19 -0300

    Refactoring class GcpToolkit

--- utils/gcp_operations.py
+++ utils/gcp_operations.py
@@ -7,23 +7,47 @@ import pandas_gbq as pd
 os.environ["GCLOUD_PROJECT"] = GCLOUD_PROJECT
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
 
-class Gcp_Toolkit:
 
-    def _client_storage():
-        storage_client = storage.Client()
-        return storage_client
-    
-    def _client_bigquery():
-        bigquery_client = bigquery.Client()
-        return bigquery_client    
+class GcpToolkit:
+    def __init__(self):
+        self.storage_client = storage.Client()
+        self.bigquery_client = bigquery.Client()
 
-    def get_name_columns_table(self, dataset:str, table_name:str)->str:
-        QUERY = f"SELECT * FROM `{dataset}.{table_name}` LIMIT 1"
-        df_sample = pd.read_gbq(QUERY)
-        columns_str = ", ".join(list(df_sample.columns))
-        return columns_str
-    
-    def get_sample_table_bigquery(self, dataset:str, table_name:str)->str:
-        QUERY = f"SELECT * FROM `{dataset}.{table_name}` LIMIT 10"
-        df_sample = pd.read_gbq(QUERY)        
-        return df_sample.to_string()
+    def get_name_columns_table(self, dataset: str, table_name: str) -> str:
+        """
+        Get names of columns in a BigQuery table.
+
+        Args:
+            dataset (str): The dataset ID.
+            table_name (str): The table name.
+
+        Returns:
+            str: A string containing column names separated by commas.
+        """
+        try:
+            query = f"SELECT * FROM `{dataset}.{table_name}` LIMIT 1"
+            df_sample = pd.read_gbq(query)
+            columns_str = ", ".join(list(df_sample.columns))
+            return columns_str
+        except Exception as e:
+            print(f"Error in get_name_columns_table: {str(e)}")
+            raise e
+
+    def get_sample_table_bigquery(self, dataset: str, table_name: str) -> str:
+        """
+        Get a sample of data from a BigQuery table.
+
+        Args:
+            dataset (str): The dataset ID.
+            table_name (str): The table name.
+
+        Returns:
+            str: A string representation of the sample data.
+        """
+        try:
+            query = f"SELECT * FROM `{dataset}.{table_name}` LIMIT 10"
+            df_sample = pd.read_gbq(query)        
+            return df_sample.to_string()
+        except Exception as e:
+            print(f"Error in get_sample_table_bigquery: {str(e)}")
+            raise e
