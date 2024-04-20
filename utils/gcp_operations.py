822cc7ec69c2545fb75edf07d65449ac93318730
Phillipe Santos <phillipefs@msn.com>
2024-04-20 11:27:22 -0300

    create execute_update_descriptions method

--- utils/gcp_operations.py
+++ utils/gcp_operations.py
@@ -2,6 +2,7 @@
 import os
 from utils.credentials import GCLOUD_PROJECT, GOOGLE_APPLICATION_CREDENTIALS
 from google.cloud import storage, bigquery
+from google.cloud.bigquery import SchemaField
 import pandas_gbq as pd
 from pandas import DataFrame
 
@@ -46,9 +47,54 @@ class GcpToolkit:
             str: A string representation of the sample data.
         """
         try:
-            query = f"SELECT * FROM `{dataset}.{table_name}` LIMIT 50"
+            query = f"SELECT * FROM `{dataset}.{table_name}` LIMIT 30"
             df_sample = pd.read_gbq(query)        
             return df_sample
         except Exception as e:
             print(f"Error in get_sample_table_bigquery: {str(e)}")
             raise e
+        
+     
+    def execute_update_descriptions(self, dataset, table_name, json_descriptions)->None:
+        """
+        Updates the descriptions of the fields in a BigQuery table schema based on a provided JSON.
+
+        Args:
+            - dataset (str): The name of the dataset containing the table.
+            - table_name (str): The name of the table to update.
+            - json_descriptions (dict): A dictionary where the keys are field names and the values are the descriptions to update.
+
+        Returns:
+            None: This function does not return anything.
+        """
+
+        try:
+     
+            client = bigquery.Client()
+            table_ref = client.dataset(dataset).table(table_name)
+            table = client.get_table(table_ref)
+
+            # Update Schema
+            new_schema = []
+            for field in table.schema:
+                if field.name in json_descriptions:
+                    # Only update the description if the field name exists in json_descriptions
+                    new_field_description = json_descriptions.get(field.name, field.description)
+                else:
+                    new_field_description = field.description
+                    
+                new_field = SchemaField(
+                    name=field.name, 
+                    field_type=field.field_type, 
+                    mode=field.mode, 
+                    description=new_field_description, 
+                    fields=field.fields
+                )
+                new_schema.append(new_field)
+
+            table.schema = new_schema
+            client.update_table(table, ["schema"])
+
+        except Exception as e:
+            raise Exception(f"An unexpected error occurred: {e}")
+
