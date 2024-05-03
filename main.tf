b8af39b7acb4e36511cb666a29f92b32a44449bd
Phillipe Santos <phillipefs@msn.com>
2024-05-02 21:22:02 -0300

    create terraform setup and workflow

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "static" {
+ name          = "data-dictionary-bigquery"
+ location      = "us-central1"
+ storage_class = "STANDARD"
+
+ uniform_bucket_level_access = true
+}
